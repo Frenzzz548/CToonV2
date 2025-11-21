ba95852b6112f5cfed5cb46ff04ac2cd510b5358
Rafi Aqila Maulana <rafiaqila2005@gmail.com>
2025-11-22 00:23:34 +0700

    pls

--- src/main/java/dao/RatingDAO.java
+++ src/main/java/dao/RatingDAO.java
@@ -1,9 +1,13 @@
 package dao;
 
-import model.Rating;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import model.Rating;
 
 public class RatingDAO {
     private Connection connection;
